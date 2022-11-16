87f449ad9270ae19dc3d0b7d41450a5115bed497
Justin Sanders <jsand155@wch133-41.cs.ucr.edu>
2022-11-15 20:05:57 -0800

    Initial Commit

---
+++ sql/scripts/create_db.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cp $DIR/../../data/*.csv /tmp/$USER/myDB/data/
+psql -h localhost -p $PGPORT $USER"_DB" < $DIR/../src/create_tables.sql
+psql -h localhost -p $PGPORT $USER"_DB" < $DIR/../src/create_indexes.sql
+psql -h localhost -p $PGPORT $USER"_DB" < $DIR/../src/load_data.sql
+
