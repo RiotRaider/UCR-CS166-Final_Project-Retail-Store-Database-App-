af622677bbaf937f272273e7c44ee56f3ef3c8cb
Rodrigo Maroto Cano <rmaro008@wch133-25.cs.ucr.edu>
2022-11-29 19:28:15 -0800

    Added trigger on placeOrder

--- java/src/Retail.java
+++ java/src/Retail.java
@@ -579,12 +579,9 @@ public class Retail {
             break;
          }while(true);
          
-         //Insert into Orders table
+         //Insert into Orders table; the product table is updated using a trigger
          String q3 = String.format("INSERT INTO Orders (customerID, storeID, productName, unitsOrdered, orderTime) VALUES (%s, %d, '%s', %d, DATE_TRUNC('second', CURRENT_TIMESTAMP::timestamp))", esql.userID, store, productName, units);
          esql.executeUpdate(q3);
-         //Update Product table
-         String q4 = String.format("UPDATE Product SET numberOfUnits = numberOfUnits - %d WHERE storeID = '%d' AND productName = '%s'", units, store, productName);
-         esql.executeUpdate(q4);
       }
       catch(Exception e){
          System.err.println(e.getMessage());
